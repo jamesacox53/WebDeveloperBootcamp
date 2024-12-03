e53caccbabae3346519d01d77c3eba577d950945
jamesacox53 <jamesacox53@gmail.com>
2024-12-03 17:50:12 +0000

    Completed Variables & Let

---
+++ 14-JavaScriptBasics/04-Variables&Let/script.js
@@ -0,0 +1,14 @@
+let year = 1985;
+console.log(year);
+
+let numHens = 5;
+let numRoosters = 1;
+
+let numChickens = numHens + numRoosters;
+console.log(numChickens);
+
+numHens = numHens + 1;
+console.log(numHens);
+
+numChickens = numHens + numRoosters;
+console.log(numChickens);
