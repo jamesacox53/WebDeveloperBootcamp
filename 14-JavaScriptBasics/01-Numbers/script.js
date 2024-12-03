48cb746c886a86f03414081a987aae4b14f2d5a4
jamesacox53 <jamesacox53@gmail.com>
2024-12-03 17:25:48 +0000

    Completed JavaScript Numbers

---
+++ 14-JavaScriptBasics/01-Numbers/script.js
@@ -0,0 +1,17 @@
+console.log("Hello World!");
+
+console.log(1);
+console.log(89);
+console.log(324.5234);
+console.log(-2);
+console.log(-2.12312312312);
+console.log(1.999999999999999);
+// This becomes 2, numbers get a certain amount of space in memory.
+// They're not permitted to take up an infinite amount of space.
+// There's a hard limit.
+console.log(1.9999999999999999);
+console.log(4 + 8);
+console.log(3.5 * 2.1);
+console.log(2 / 5);
+console.log(9 % 2);
+console.log(2 ** 4);
