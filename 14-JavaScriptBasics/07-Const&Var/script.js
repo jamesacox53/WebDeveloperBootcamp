925a40e4c7f7897b7622e03c9e05ec90bf2af214
jamesacox53 <jamesacox53@gmail.com>
2024-12-04 16:50:58 +0000

    Completed Const & Var

---
+++ 14-JavaScriptBasics/07-Const&Var/script.js
@@ -0,0 +1,11 @@
+const luckyNum = 26;
+console.log(luckyNum);
+
+const feetInMile = 5280;
+console.log(feetInMile);
+
+var runDistance = 26.2;
+console.log(runDistance);
+
+runDistance += 1;
+console.log(runDistance);
