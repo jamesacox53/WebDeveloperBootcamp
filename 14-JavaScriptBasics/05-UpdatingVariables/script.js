1cba02346a30c8e2b84fd962bb04158fc2eab010
jamesacox53 <jamesacox53@gmail.com>
2024-12-04 16:34:54 +0000

    Completed Updating Variables

---
+++ 14-JavaScriptBasics/05-UpdatingVariables/script.js
@@ -0,0 +1,22 @@
+let score = 0;
+
+score = score + 5;
+console.log(score);
+
+score += 5;
+console.log(score);
+
+score -= 5;
+console.log(score);
+
+score *= 5;
+console.log(score);
+
+score /= 2;
+console.log(score);
+
+score++;
+console.log(score);
+
+score--;
+console.log(score);
