bab6230341de1fbf88fd9c5d9da335a509c01940
jamesacox53 <jamesacox53@gmail.com>
2024-12-06 17:20:03 +0000

    Completed Indices & Length

---
+++ 15-JavaScriptStrings/02-Indices&Length/script.js
@@ -0,0 +1,13 @@
+let animal = "Octopus";
+console.log(animal[0]);
+console.log(animal[1]);
+console.log(animal[99]);
+
+let phone = "(231)345-1344";
+console.log(phone[0]);
+
+console.log(animal.length);
+
+console.log("Hello" + " " + "World!");
+
+console.log("1" + "2");
