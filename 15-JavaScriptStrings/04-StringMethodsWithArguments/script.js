031595c447f46fe1e60a66a8b0f9872d419c4b6a
jamesacox53 <jamesacox53@gmail.com>
2024-12-07 17:43:39 +0000

    Completed String Methods With Arguments

---
+++ 15-JavaScriptStrings/04-StringMethodsWithArguments/script.js
@@ -0,0 +1,14 @@
+let tvShow = 'catdog';
+console.log(tvShow.indexOf('cat'));
+console.log(tvShow.indexOf('dog'));
+console.log(tvShow.indexOf('z'));
+
+let sentence = 'haha that is so funny!';
+console.log(sentence.slice(5));
+console.log(sentence.slice(5, 9));
+console.log(sentence.slice(-1));
+
+let annoyingLaugh = 'teehee so funny! teehee!';
+console.log(annoyingLaugh.replaceAll('teehee', 'haha'));
+
+console.log("ha".repeat(10));
