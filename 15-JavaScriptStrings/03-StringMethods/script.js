f24bd476efc8e14262a0daa4f6d8fd831b9b9035
jamesacox53 <jamesacox53@gmail.com>
2024-12-07 17:16:21 +0000

    Completed String Methods

---
+++ 15-JavaScriptStrings/03-StringMethods/script.js
@@ -0,0 +1,9 @@
+let msg = 'I am the king';
+let yellMsg = msg.toUpperCase();
+console.log(yellMsg);
+
+let angry = 'LeAvE mE aLoNe!';
+console.log(angry.toLowerCase());
+
+let greeting = '     Hello there        ';
+console.log(greeting.trim().toUpperCase());
