aa4f3b08cda51d2b01ac4fa673c081653f6edfdb
jamesacox53 <jamesacox53@gmail.com>
2024-12-08 17:50:30 +0000

    Completed Random Numbers & The Math Object

---
+++ 15-JavaScriptStrings/06-RandomNumbers&TheMathObject/script.js
@@ -0,0 +1,8 @@
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.floor(23.90));
+console.log(Math.ceil(34.1));
+console.log(Math.random());
+
+const randomInt = Math.floor(Math.random() * 10) + 1;
+console.log(randomInt);
