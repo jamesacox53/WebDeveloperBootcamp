71fe79981ea84fcda40e1fde5ff14e3bbda45f8f
jamesacox53 <jamesacox53@gmail.com>
2024-12-09 17:51:50 +0000

    Completed Equality: Triple Vs. Double Equals

---
+++ 16-JavaScriptDecisionMaking/02-EqualityTripleVsDoubleEquals/script.js
@@ -0,0 +1,11 @@
+console.log(1 == 1);
+console.log(1 == '1');
+console.log(0 == '');
+console.log(0 == false);
+console.log(null == undefined);
+
+console.log(2 === '2');
+
+console.log(1 != 2);
+console.log(1 != '1');
+console.log(1 !== '1');
