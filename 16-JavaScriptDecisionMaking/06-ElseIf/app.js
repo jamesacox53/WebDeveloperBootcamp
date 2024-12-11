cd378fba92ddc3891d67fc552817548f9b787bab
jamesacox53 <jamesacox53@gmail.com>
2024-12-11 18:05:56 +0000

    Completed Else-If

---
+++ 16-JavaScriptDecisionMaking/06-ElseIf/app.js
@@ -0,0 +1,23 @@
+const dayOfWeekStr = 'Friday';
+
+if (dayOfWeekStr === 'Monday') {
+    console.log('I hate Mondays!');
+
+} else if (dayOfWeekStr === 'Saturday') {
+    console.log('I love Saturdays!');
+
+} else if (dayOfWeekStr === 'Friday') {
+    console.log('Fridays are decent, especially after work.');
+}
+
+const ageInt = 21;
+
+if (ageInt < 5) {
+    console.log('You are a baby. You get in for Free.');
+
+} else if (ageInt < 10) {
+    console.log('You are a child. You pay $10.');
+
+} else if (ageInt < 65) {
+    console.log('You are an adult. You pay $20.');
+}
