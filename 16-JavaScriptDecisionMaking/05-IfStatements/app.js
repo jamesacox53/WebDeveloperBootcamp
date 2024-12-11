15e14e90b57a9ae1eacad65636c4813fa6b379c1
jamesacox53 <jamesacox53@gmail.com>
2024-12-11 17:46:03 +0000

    Completed If Statements

---
+++ 16-JavaScriptDecisionMaking/05-IfStatements/app.js
@@ -0,0 +1,13 @@
+let num = Math.random();
+
+console.log("Before conditionals");
+if (num < 0.5) {
+    console.log("Your number is less than 0.5");
+    console.log(num);
+}
+
+if (num >= 0.5) {
+    console.log("Your number is greater than 0.5");
+    console.log(num);
+}
+console.log("After conditionals");
