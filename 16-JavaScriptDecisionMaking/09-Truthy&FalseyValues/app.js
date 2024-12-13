10c75ec6a330d9576ae114e6d0a7214a3d1db7cb
jamesacox53 <jamesacox53@gmail.com>
2024-12-13 15:35:02 +0000

    Completed Truth-y & False-y Values

---
+++ 16-JavaScriptDecisionMaking/09-Truthy&FalseyValues/app.js
@@ -0,0 +1,29 @@
+const userInput = prompt("Enter something");
+
+if (userInput) {
+    console.log("Truthy!");
+
+} else {
+    console.log("Falsey!");
+}
+
+if (0) {
+    console.log("Truthy!");
+
+} else {
+    console.log("Falsey!");
+}
+
+if (1) {
+    console.log("Truthy!");
+
+} else {
+    console.log("Falsey!");
+}
+
+if (undefined) {
+    console.log("Truthy!");
+
+} else {
+    console.log("Falsey!");
+}
