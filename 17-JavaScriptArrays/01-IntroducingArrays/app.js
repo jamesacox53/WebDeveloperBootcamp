e524462f9a5515f3fa26e64d6ff1dc5206dc59c8
jamesacox53 <jamesacox53@gmail.com>
2024-12-15 16:33:15 +0000

    Completed Introducing Arrays

---
+++ 17-JavaScriptArrays/01-IntroducingArrays/app.js
@@ -0,0 +1,9 @@
+let colors = [];
+console.log(colors);
+
+let days = ['Monday', 'Tuesday', 'Wednesday'];
+console.log(days);
+console.log(days.length);
+
+let kitchenSink = [true, undefined, 12, 9.999, NaN, false, null, 'Hi'];
+console.log(kitchenSink);
