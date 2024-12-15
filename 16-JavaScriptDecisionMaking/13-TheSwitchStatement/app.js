af5932609310965c55a0f8e47f3091f9ffc736af
jamesacox53 <jamesacox53@gmail.com>
2024-12-15 16:16:27 +0000

    Completed The Switch Statement Is ...A Lot

---
+++ 16-JavaScriptDecisionMaking/13-TheSwitchStatement/app.js
@@ -0,0 +1,32 @@
+const day = 7;
+
+switch (day) {
+    case 1: {
+        console.log('Monday');
+        break;
+    }
+    case 2: {
+        console.log('Tuesday');
+        break;
+    }
+    case 3: {
+        console.log('Tuesday');
+        break;
+    }
+    case 4: {
+        console.log('Tuesday');
+        break;
+    }
+    case 5: {
+        console.log('Tuesday');
+        break;
+    }
+    case 6:
+    case 7: {
+        console.log('Weekend');
+        break;
+    }
+    default: {
+        console.log("I don't know that.");
+    }
+}
