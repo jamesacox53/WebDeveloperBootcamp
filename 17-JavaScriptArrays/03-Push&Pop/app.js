888ceff3b7234161cfcf182db4b75ab0c799b9be
jamesacox53 <jamesacox53@gmail.com>
2024-12-16 17:52:49 +0000

    Completed Push & Pop

---
+++ 17-JavaScriptArrays/03-Push&Pop/app.js
@@ -0,0 +1,11 @@
+let movieLine = ['tom', 'nancy'];
+movieLine.push('pablo');
+movieLine.push('oliver');
+movieLine.push('eva');
+movieLine.push('harry');
+movieLine.push('hermione');
+console.log(movieLine);
+
+let person = movieLine.pop();
+console.log(person);
+console.log(movieLine);
