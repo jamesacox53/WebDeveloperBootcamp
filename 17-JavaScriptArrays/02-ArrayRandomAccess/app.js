81780c9ffa9c6c9c03eb6769fa5f679f47b34dc2
jamesacox53 <jamesacox53@gmail.com>
2024-12-16 17:32:59 +0000

    Completed Array Random Access

---
+++ 17-JavaScriptArrays/02-ArrayRandomAccess/app.js
@@ -0,0 +1,21 @@
+let days = ['Monday', 'Tuesday', 'Wednesday'];
+console.log(days);
+console.log(days.length);
+console.log(days[0]);
+console.log(days[2][0]);
+
+let colors = ['rad', 'orange', 'green', 'yellow'];
+console.log(colors);
+
+colors[0] = 'red';
+colors[2] = 'yellow';
+colors[3] = 'green';
+colors[4] = 'blue';
+colors[10] = 'indigo';
+console.log(colors);
+
+let beatles = ['paul', 'john', 'george'];
+console.log(beatles);
+
+beatles[beatles.length] = 'ringo';
+console.log(beatles);
