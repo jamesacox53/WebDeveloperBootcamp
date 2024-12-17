11394fb6831a653bec7c6e12e38b2032c37ff038
jamesacox53 <jamesacox53@gmail.com>
2024-12-17 17:47:38 +0000

    Completed Concat, indexOf, includes & reverse

---
+++ 17-JavaScriptArrays/05-ConcatIndexOfIncludes&Reverse/app.js
@@ -0,0 +1,17 @@
+let cats = ['blue', 'kitty'];
+let dogs = ['rusty', 'wyatt'];
+
+let pets = cats.concat(dogs);
+console.log(pets);
+console.log(cats);
+console.log(dogs);
+
+console.log(cats.includes('blue'));
+console.log(cats.includes('wyatt'));
+
+console.log(cats.indexOf('blue'));
+console.log(cats.indexOf('wyatt'));
+
+console.log(pets);
+pets.reverse();
+console.log(pets);
