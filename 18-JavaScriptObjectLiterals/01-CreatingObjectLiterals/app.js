3e0e25f9d786b0050fb9085bc64bee38066a6775
jamesacox53 <jamesacox53@gmail.com>
2024-12-20 17:44:47 +0000

    Completed Creating Object Literals

---
+++ 18-JavaScriptObjectLiterals/01-CreatingObjectLiterals/app.js
@@ -0,0 +1,14 @@
+const person = {
+    firstName: 'Mick',
+    lastName: 'Jagger'
+};
+
+console.log(person);
+
+const kitchenSink = {
+    favNum: 1231231,
+    isFunny: true,
+    colors: ['red', 'orange']
+};
+
+console.log(kitchenSink);
