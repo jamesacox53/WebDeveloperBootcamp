8b878a713b3633b8ef1d6c99cf04a7d22e755675
jamesacox53 <jamesacox53@gmail.com>
2024-12-22 17:39:37 +0000

    Completed Nesting Arrays & Objects

---
+++ 18-JavaScriptObjectLiterals/04-NestingArrays&Objects/app.js
@@ -0,0 +1,15 @@
+const comments = [
+    {
+        userName: 'Tammy',
+        text: 'Hello World',
+        votes: 9
+    },
+    {
+        userName: 'FishBoi',
+        text: 'Glub Glub',
+        votes: 12387
+    }
+];
+
+console.log(comments);
+console.log(comments[1]['text']);
