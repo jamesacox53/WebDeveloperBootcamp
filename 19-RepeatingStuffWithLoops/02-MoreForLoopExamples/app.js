ab03aed7d4f5329eed9317548faecdd124a1b0e9
jamesacox53 <jamesacox53@gmail.com>
2024-12-23 17:43:28 +0000

    Completed More For Loops Examples

---
+++ 19-RepeatingStuffWithLoops/02-MoreForLoopExamples/app.js
@@ -0,0 +1,15 @@
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i);
+}
+
+console.log('---------------');
+
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(i);
+}
+
+console.log('---------------');
+
+for (let i = 10; i <= 1000; i = i * 10) {
+    console.log(i);
+}
