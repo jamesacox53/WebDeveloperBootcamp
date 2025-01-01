e71e0d7a52dd44434c015407c3752c727498cc6c
jamesacox53 <jamesacox53@gmail.com>
2025-01-01 13:12:51 +0000

    Completed Function Scope

---
+++ 21-LevelingUpOurFunctions/01-FunctionScope/app.js
@@ -0,0 +1,18 @@
+function collectEggs() {
+    let totalEggs = 6;
+    console.log(totalEggs);
+}
+
+collectEggs();
+
+let bird = 'Scarlet Macaw';
+
+function birdWatch() {
+    let bird = 'Great Blue Heron';
+
+    console.log(bird);
+}
+
+console.log(bird);
+birdWatch();
+console.log(bird);
