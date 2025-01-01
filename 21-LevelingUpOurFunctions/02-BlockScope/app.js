e20e869aa7cffaf5002866d91b8b946d15539f21
jamesacox53 <jamesacox53@gmail.com>
2025-01-01 13:27:45 +0000

    Completed Block Scope

---
+++ 21-LevelingUpOurFunctions/02-BlockScope/app.js
@@ -0,0 +1,31 @@
+let radius = 8;
+
+if (radius > 0) {
+    const PI = 3.14159;
+}
+
+console.log(radius);
+// console.log(PI);
+
+for (let i = 0; i < 5; i++) {
+    let msg = 'Hi!';
+}
+
+// console.log(i);
+// console.log(msg);
+
+let radius2 = 8;
+
+if (radius2 > 0) {
+    var PI2 = 3.14159;
+}
+
+console.log(radius2);
+console.log(PI2);
+
+for (var i2 = 0; i2 < 5; i2++) {
+    var msg2 = 'Hi!';
+}
+
+console.log(i2);
+console.log(msg2);
