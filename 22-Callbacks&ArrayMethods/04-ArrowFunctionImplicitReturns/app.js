50080ab52f5348fadea57a9d45cc0f4626e02214
jamesacox53 <jamesacox53@gmail.com>
2025-01-07 17:07:35 +0000

    Completed Arrow Function Implicit Returns

---
+++ 22-Callbacks&ArrayMethods/04-ArrowFunctionImplicitReturns/app.js
@@ -0,0 +1,13 @@
+const add = (x, y) => x + y;
+
+console.log(add(5, 7));
+
+const square = x => x * x;
+
+console.log(square(5));
+
+const rollDie = () => Math.floor(Math.random() * 6) + 1;
+
+console.log(rollDie());
+console.log(rollDie());
+console.log(rollDie());
