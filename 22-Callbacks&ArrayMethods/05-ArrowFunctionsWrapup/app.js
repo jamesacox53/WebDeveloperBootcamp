dd5a019d1f37ac8ca0de3eb0d18e3732b5e5dacc
jamesacox53 <jamesacox53@gmail.com>
2025-01-07 17:14:47 +0000

    Completed Arrow Functions Wrapup

---
+++ 22-Callbacks&ArrayMethods/05-ArrowFunctionsWrapup/app.js
@@ -0,0 +1,28 @@
+const numbersArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+const doublesArr = numbersArr.map(num => num * 2);
+
+console.log(doublesArr);
+
+const moviesArr = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+];
+
+const titlesArr = moviesArr.map(movieObj => movieObj.title);
+
+console.log(titlesArr);
