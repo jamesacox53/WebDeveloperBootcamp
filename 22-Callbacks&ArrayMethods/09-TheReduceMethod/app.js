de974e211f92164c4659b4f2f8fc287b9bf762b4
jamesacox53 <jamesacox53@gmail.com>
2025-01-09 17:22:32 +0000

    Completed The Notorious Reduce Method

---
+++ 22-Callbacks&ArrayMethods/09-TheReduceMethod/app.js
@@ -0,0 +1,81 @@
+const pricesArr = [9.99, 1.50, 19.99, 49.99, 30.50];
+
+const totalNum = pricesArr.reduce((acc, elem) => {
+    return acc + elem;
+});
+
+console.log(totalNum);
+
+const minPriceNum = pricesArr.reduce((acc, elem) => {
+    if (acc < elem) {
+        return acc;
+    
+    } else {
+        return elem;
+    }
+});
+
+console.log(minPriceNum);
+
+const moviesArr = [
+    {
+        title: 'Amadeus',
+        score: 99,
+        year: 1984
+    },
+    {
+        title: 'Sharknado',
+        score: 35,
+        year: 2013
+    },
+    {
+        title: '13 Going On 30',
+        score: 70,
+        year: 2004
+    },
+    {
+        title: 'Stand By Me',
+        score: 85,
+        year: 1986
+    },
+    {
+        title: 'Waterworld',
+        score: 62,
+        year: 1995
+    },
+    {
+        title: 'Jingle All The Way',
+        score: 71,
+        year: 1996
+    },
+    {
+        title: 'Parasite',
+        score: 95,
+        year: 2019
+    },
+    {
+        title: 'Notting Hill',
+        score: 77,
+        year: 1999
+    },
+    {
+        title: 'Alien',
+        score: 90,
+        year: 1979
+    }
+];
+
+const bestMovieObj = moviesArr.reduce((acc, elem) => {
+    if (acc.score > elem.score) {
+        return acc;
+    
+    } else {
+        return elem;
+    }
+});
+
+console.log(bestMovieObj);
+
+const evensArr = [2, 4, 6, 8];
+const evensSumInt = evensArr.reduce((a, e) => a + e, 100);
+console.log(evensSumInt);
