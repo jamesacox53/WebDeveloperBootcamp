d0bc41a4a382b108014cc2d6ed0032175b1f9cd4
jamesacox53 <jamesacox53@gmail.com>
2025-01-10 17:45:32 +0000

    Completed Spread in Function Calls

---
+++ 23-NewerJavaScriptFeatures/02-SpreadInFunctionCalls/app.js
@@ -0,0 +1,10 @@
+const numsArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+const maxNum = Math.max(...numsArr);
+console.log(maxNum);
+
+console.log(...numsArr);
+
+console.log('Hello');
+
+console.log(...'Hello');
