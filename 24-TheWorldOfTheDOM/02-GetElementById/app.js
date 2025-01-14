ab49fb85754c24f02390fc06288bc736b0962d6a
jamesacox53 <jamesacox53@gmail.com>
2025-01-14 17:35:11 +0000

    Completed getElementById

---
+++ 24-TheWorldOfTheDOM/02-GetElementById/app.js
@@ -0,0 +1,8 @@
+const doesntExist = document.getElementById('chicken');
+console.dir(doesntExist);
+
+const bannerElem = document.getElementById('banner');
+console.dir(bannerElem);
+
+const tocElem = document.getElementById('toc');
+console.dir(tocElem);
