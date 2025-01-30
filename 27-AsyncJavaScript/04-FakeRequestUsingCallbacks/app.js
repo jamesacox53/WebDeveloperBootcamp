10615f7d525839211cd85af2c5cd9fabcb6c2a61
jamesacox53 <jamesacox53@gmail.com>
2025-01-30 17:51:14 +0000

    Completed Demo: fakeRequest Using Promises

--- 27-AsyncJavaScript/04-FakeRequestUsingCallbacks/app.js
+++ 27-AsyncJavaScript/04-FakeRequestUsingCallbacks/app.js
@@ -1,4 +1,3 @@
-// THE CALLBACK VERSION
 const fakeRequestCallback = (url, success, failure) => {
     const delay = Math.floor(Math.random() * 4500) + 500;
     setTimeout(() => {
@@ -8,20 +7,6 @@ const fakeRequestCallback = (url, success, failure) => {
             success(`Here is your fake data from ${url}`)
         }
     }, delay)
-}
-
-// THE PROMISE VERSION 
-const fakeRequestPromise = (url) => {
-    return new Promise((resolve, reject) => {
-        const delay = Math.floor(Math.random() * (4500)) + 500;
-        setTimeout(() => {
-            if (delay > 4000) {
-                reject('Connection Timeout :(')
-            } else {
-                resolve(`Here is your fake data from ${url}`)
-            }
-        }, delay)
-    })
 }
 
 fakeRequestCallback('books.com/page1',
---
+++ 27-AsyncJavaScript/05-FakeRequestUsingPromises/app.js
@@ -0,0 +1,34 @@
+const fakeRequestPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 4000) {
+                reject('Connection Timeout :(')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    })
+}
+
+fakeRequestPromise('yelp.com/api/coffee/page1')
+    .then(() => {
+        console.log("IT WORKED!!!!!! (page1)")
+        fakeRequestPromise('yelp.com/api/coffee/page2')
+            .then(() => {
+                console.log("IT WORKED!!!!!! (page2)")
+                fakeRequestPromise('yelp.com/api/coffee/page3')
+                    .then(() => {
+                        console.log("IT WORKED!!!!!! (page3)")
+                    })
+                    .catch(() => {
+                        console.log("OH NO, ERROR!!! (page3)")
+                    })
+            })
+            .catch(() => {
+                console.log("OH NO, ERROR!!! (page2)")
+            })
+    })
+    .catch(() => {
+        console.log("OH NO, ERROR!!! (page1)")
+    })
