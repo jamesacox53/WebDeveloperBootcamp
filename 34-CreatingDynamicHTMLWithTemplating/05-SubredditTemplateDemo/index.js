6ad9265bbc360424474cbd7aee6cfcf4f904017a
jamesacox53 <jamesacox53@gmail.com>
2025-03-13 17:45:47 +0000

    Completed Subreddit Template Demo

---
+++ 34-CreatingDynamicHTMLWithTemplating/05-SubredditTemplateDemo/index.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/rand', (req, res) => {
+    const randInt = Math.floor(Math.random() * 10) + 1;
+
+    const optionsObj = {
+        randInt: randInt
+    };
+    
+    res.render('random.ejs', optionsObj);
+});
+
+app.get('/r/:subreddit', (req, res) => {
+    const subredditStr = req.params.subreddit;
+
+    const optionsObj = {
+        subredditStr: subredditStr
+    };
+    
+    res.render('subreddit.ejs', optionsObj);
+});
+
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+});
+
+
+app.listen(3000, () => {
+    console.log("Listening on Port 3000");   
+});
