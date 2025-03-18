fc5f069f7cad6bb1e56b424ff73311bdb7246e20
jamesacox53 <jamesacox53@gmail.com>
2025-03-18 17:52:17 +0000

    Completed EJS & Partials

---
+++ 34-CreatingDynamicHTMLWithTemplating/11-EJS&Partials/index.js
@@ -0,0 +1,70 @@
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+const redditDataObj = require('./data.json');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/cats', (req, res) => {
+    const catArr = ['Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'];
+
+    const optionsObj = {
+        name: 'All Cats',
+        catArr: catArr
+    };
+
+    res.render('cats.ejs', optionsObj);
+});
+
+app.get('/rand', (req, res) => {
+    const randInt = Math.floor(Math.random() * 10) + 1;
+
+    const optionsObj = {
+        randInt: randInt,
+        name: 'Random'
+    };
+    
+    res.render('random.ejs', optionsObj);
+});
+
+app.get('/r/:subreddit', (req, res) => {
+    const subredditStr = req.params.subreddit;
+
+    const dataObj = redditDataObj[subredditStr];
+
+    if (dataObj) {
+        const optionsObj = {
+            subredditStr: subredditStr,
+            ...dataObj
+        };
+    
+        res.render('subreddit.ejs', optionsObj);
+    
+    } else {
+        const optionsObj = {
+            name: 'Not Found',
+            subredditStr: subredditStr
+        };
+    
+        res.render('notfound.ejs', optionsObj);
+    }
+});
+
+app.get('/', (req, res) => {
+    const optionsObj = {
+        name: 'Home'
+    };
+
+    res.render('home.ejs', optionsObj);
+});
+
+
+app.listen(3000, () => {
+    console.log("Listening on Port 3000");   
+});
