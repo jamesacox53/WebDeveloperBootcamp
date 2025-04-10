4adcf9f98665228f45aa000f1842e94d71d5d7a4
jamesacox53 <jamesacox53@gmail.com>
2025-04-10 18:05:52 +0100

    Completed RESTful Comments New

---
+++ 35-DefiningRESTfulRoutes/06-RESTfulCommentsNew/index.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// GET /comments - list all comments
+// POST /comments - Create a new comment
+// GET /comments/:id - Get one comment (using ID)
+// PATCH /comments/:id - Update one comment
+// DELETE /comments/:id - Destroy one comment
+
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+];
+
+app.get('/comments', (req, res) => {
+
+    const optionsObj = {
+        comments: comments
+    };
+
+    res.render('comments/index.ejs', optionsObj);
+});
+
+app.get('/comments/new', (req, res) => {
+
+    const optionsObj = {
+        comments: comments
+    };
+
+    res.render('comments/new.ejs', optionsObj);
+});
+
+app.post('/comments', (req, res) => {
+    const {username, comment} = req.body;
+
+    comments.push({username, comment});
+    
+    res.send("It worked");
+});
+
+app.post('/tacos', (req, res) => {
+    const { meat, qty } = req.body;
+    res.send(`Ok, here are your ${qty} ${meat} tacos`);
+});
+
+app.listen(3000, () => {
+    console.log('On 3000');
+});
