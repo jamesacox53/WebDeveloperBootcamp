d5e97fb116960d4645e186eff8f22ba484607ed5
jamesacox53 <jamesacox53@gmail.com>
2025-04-11 12:46:17 +0100

    Completed RESTful Comments Show

---
+++ 35-DefiningRESTfulRoutes/08-RESTfulCommentsShow/index.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// GET /comments - list all comments
+// POST /comments - Create a new comment
+// GET /comments/:id - Get one comment (using ID)
+// PATCH /comments/:id - Update one comment
+// DELETE /comments/:id - Destroy one comment
+
+const comments = [
+    {
+        id: 1,
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {
+        id: 2,
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        id: 3,
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        id: 4,
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+];
+
+app.get('/comments', (req, res) => {
+
+    const optionsObj = {
+        comments: comments
+    };
+
+    res.render('comments/index.ejs', optionsObj);
+});
+
+app.get('/comments/new', (req, res) => {
+
+    const optionsObj = {
+        comments: comments
+    };
+
+    res.render('comments/new.ejs', optionsObj);
+});
+
+app.post('/comments', (req, res) => {
+    const { username, comment } = req.body;
+
+    comments.push({ username, comment });
+    
+    res.redirect('/comments');
+});
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const idInt = parseInt(id);
+
+    const comment = comments.find(c => c.id === idInt);
+
+    const optionsObj = {
+        comment: comment
+    };
+
+    res.render('comments/show.ejs', optionsObj);
+});
+
+app.post('/tacos', (req, res) => {
+    const { meat, qty } = req.body;
+    res.send(`Ok, here are your ${qty} ${meat} tacos`);
+});
+
+app.listen(3000, () => {
+    console.log('On 3000');
+});
