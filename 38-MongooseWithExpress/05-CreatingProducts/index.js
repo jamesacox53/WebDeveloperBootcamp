426fafb52919b9d868a7f5d9c5dac4cab63834c5
jamesacox53 <jamesacox53@gmail.com>
2025-05-02 12:45:52 +0100

    Completed Creating Products

---
+++ 38-MongooseWithExpress/05-CreatingProducts/index.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+const Product = require('./models/product.js');
+
+app.get('/products', async (req, res) => {
+    const productArr = await Product.find({});
+    
+    const optionsObj = {
+        productArr: productArr
+    };
+
+    res.render('products/index.ejs', optionsObj);
+});
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new.ejs');
+});
+
+app.post('/products', async (req, res) => {
+    const newProductObj = new Product(req.body);
+    await newProductObj.save();
+
+    res.redirect(`/products/${newProductObj._id}`);
+});
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    
+    const productObj = await Product.findById(id);
+    
+    const optionsObj = {
+        productObj: productObj
+    };
+
+    res.render('products/show.ejs', optionsObj);
+});
+
+app.listen(3000, async () => {
+    try {
+        await main();
+    
+    } catch(err) {
+        console.log(err);
+    }
+});
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/farmStand');
+}
