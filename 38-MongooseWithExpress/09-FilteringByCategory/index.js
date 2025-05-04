6fb5676dfabca903999243b8faf9b0dc3f3e1e71
jamesacox53 <jamesacox53@gmail.com>
2025-05-04 13:22:57 +0100

    Completed BONUS: Filtering By Category

---
+++ 38-MongooseWithExpress/09-FilteringByCategory/index.js
@@ -0,0 +1,105 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
+
+const Product = require('./models/product.js');
+const categoryArr = ['Fruit', 'Vegetable', 'Dairy'];
+
+app.get('/products', async (req, res) => {
+    const categoryStr = req.query.category;
+    
+    if (categoryStr) {
+        const productArr = await Product.find({ category: categoryStr });
+    
+        const optionsObj = {
+            productArr: productArr,
+            categoryStr: categoryStr
+        };
+
+        res.render('products/index.ejs', optionsObj);
+    
+    } else {
+        const productArr = await Product.find({});
+    
+        const optionsObj = {
+            productArr: productArr,
+            categoryStr: 'All'
+        };
+
+        res.render('products/index.ejs', optionsObj);
+    }
+});
+
+app.get('/products/new', (req, res) => {
+    const optionsObj = {
+        categoryArr: categoryArr
+    };
+
+    res.render('products/new.ejs', optionsObj);
+});
+
+app.post('/products', async (req, res) => {
+    const newProductObj = new Product(req.body);
+    await newProductObj.save();
+
+    res.redirect(`/products/${newProductObj._id}`);
+});
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const productObj = await Product.findById(id);
+    
+    const optionsObj = {
+        productObj: productObj
+    };
+
+    res.render('products/show.ejs', optionsObj);
+});
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const productObj = await Product.findById(id);
+    
+    const optionsObj = {
+        productObj: productObj,
+        categoryArr: categoryArr
+    };
+
+    res.render('products/edit.ejs', optionsObj);
+});
+
+app.put('/products/:id/', async (req, res) => {
+    const { id } = req.params;
+    const optionsObj = { runValidators: true, new: true  };
+    
+    const productObj = await Product.findByIdAndUpdate(id, req.body, optionsObj);
+
+    res.redirect(`/products/${productObj._id}`);
+});
+
+app.delete('/products/:id/', async (req, res) => {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+
+    res.redirect('/products');
+});
+
+app.listen(3000, async () => {
+    try {
+        await main();
+    
+    } catch(err) {
+        console.log(err);
+    }
+});
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/farmStand');
+}
