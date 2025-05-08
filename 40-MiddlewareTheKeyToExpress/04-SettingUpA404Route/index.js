4264805814e3e827860486780f5c1825b66bb07d
jamesacox53 <jamesacox53@gmail.com>
2025-05-08 11:37:19 +0100

    Completed Setting Up A 404 Route

---
+++ 40-MiddlewareTheKeyToExpress/04-SettingUpA404Route/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('tiny'));
+
+app.use((req, res, next) => {
+    console.log(req.method, req.path);
+    req.requestTime = Date.now();
+
+    next();
+});
+
+app.use('/dogs', (req, res, next) => {
+    console.log('I love dogs');
+
+    next();
+});
+
+/*
+app.use((req, res, next) => {
+    console.log("This is my first middleware!");
+    next();
+
+    console.log("This is my first middleware after next()!");
+});
+
+app.use((req, res, next) => {
+    console.log("This is my second middleware!");
+    next();
+
+    console.log("This is my second middleware after next()!");
+});
+*/
+
+app.get('/', (req, res) => {
+    console.log(`Request Date: ${req.requestTime}`);
+    res.send('Home Page');
+});
+
+app.get('/dogs', (req, res) => {
+    console.log(`Request Date: ${req.requestTime}`);
+    res.send('Woof');
+});
+
+app.use((req, res) => {
+    res.status(404).send('Not found!');
+});
+
+app.listen(3000, () => {
+    console.log("Serving on port 3000");
+});
