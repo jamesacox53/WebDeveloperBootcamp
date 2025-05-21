d8b3f7cf110f29ddcfe65e81f86f1e9716c3875c
jamesacox53 <jamesacox53@gmail.com>
2025-05-21 18:18:24 +0100

    Completed Express Router Intro

---
+++ 47-ExpressRouter&Cookies/01-ExpressRouterIntro/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const shelterRoutes = require('./routes/shelters');
+const dogRoutes = require('./routes/dogs');
+
+app.use('/shelters', shelterRoutes);
+app.use('/dogs', dogRoutes);
+
+app.listen(3000, () => {
+    console.log('Running on port 3000');
+});
