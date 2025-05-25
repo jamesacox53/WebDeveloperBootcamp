dab63507844d18cbeb2b5347f580a1fc7c4092ea
jamesacox53 <jamesacox53@gmail.com>
2025-05-25 15:19:48 +0100

    Completed Configuring Session

---
+++ 49-YelpCampRestructuring&Flash/04-ConfiguringSessions/utils/catchAsync.js
@@ -0,0 +1,12 @@
+function catchAsync(func) {
+    return async function (req, res, next) {
+        try {
+            await func(req, res, next);
+
+        } catch (err) {
+            next(err);
+        }
+    }
+}
+
+module.exports = catchAsync;
