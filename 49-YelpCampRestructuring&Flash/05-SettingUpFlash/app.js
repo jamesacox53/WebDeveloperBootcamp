fe846b60be5262b73b8ce785a020effc3e8f58bb
jamesacox53 <jamesacox53@gmail.com>
2025-05-25 15:34:28 +0100

    Completed Setting Up Flash

---
+++ 49-YelpCampRestructuring&Flash/05-SettingUpFlash/app.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+const app = express();
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+};
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+const ExpressError = require('./utils/ExpressError');
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+
+    return next();
+});
+
+const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
+
+app.all(/(.*)/, (req, res, next) => {
+    const err = new ExpressError('Page Not Found', 404);
+
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    if (!err.statusCode)
+        err.statusCode = 500;
+
+    if (!err.message)
+        err.message = 'Something went wrong';
+
+    res.status(err.statusCode).render('error', { err });
+});
+
+
+app.listen(3000, async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+        console.log("Serving on port 3000");
+
+    } catch (err) {
+        console.log(err);
+    }
+});
