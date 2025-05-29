3b0f817894f58304b84af65236d2038efd2e8bef
jamesacox53 <jamesacox53@gmail.com>
2025-05-29 16:53:48 +0100

    Completed Auth Demo: Refactoring To Model Methods

---
+++ 50-AuthenticationFromScratch/08-AuthDemoRefactoringToModelMethods/index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'notagoodsecret' }));
+
+const User = require('./models/user');
+
+function requireLogin(req, res, next) {
+    if (!req.session.userId) {
+        return res.redirect('/login');
+    }
+
+    return next();
+}
+
+app.get('/', (req, res) => {
+    res.send('This is the homepage');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = new User({
+        username: username,
+        password: password
+    });
+
+    await user.save();
+    req.session.userId = user._id;
+
+    res.redirect('/');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const foundUser = await User.findAndValidate(username, password);
+
+    if (foundUser) {
+        req.session.userId = foundUser._id;
+
+        res.redirect('/secret');
+    
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.post('/logout', async (req, res) => {
+    // req.session.userId = null;
+    req.session.destroy();
+
+    res.redirect('/login');
+});
+
+app.get('/secret', requireLogin, (req, res) => {
+    res.render('secret');
+});
+
+app.get('/topsecret', requireLogin, (req, res) => {
+    res.send('Top secret');
+});
+
+app.listen(3000, async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/authDemo');
+
+        console.log("Serving on port 3000");
+
+    } catch (err) {
+        console.log(err);
+    }
+});
