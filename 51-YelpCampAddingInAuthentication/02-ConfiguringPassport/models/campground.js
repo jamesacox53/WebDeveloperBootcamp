52a5317baa08a25571d5b0f6948dbca3303f37e7
jamesacox53 <jamesacox53@gmail.com>
2025-05-30 08:32:39 +0100

    Completed Configuring Passport

---
+++ 51-YelpCampAddingInAuthentication/02-ConfiguringPassport/models/campground.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Review = require('./review');
+
+const campgroundSchema = new Schema({
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
+
+campgroundSchema.post('findOneAndDelete', async function(doc) {
+    if (!doc || !doc.reviews) return;
+
+    await Review.deleteMany({
+        _id: {
+            $in: doc.reviews
+        }
+    });
+
+});
+
+module.exports = mongoose.model('Campground', campgroundSchema);
