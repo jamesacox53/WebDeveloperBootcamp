d6ac642e26ed7091f9a1f6ce970ccf136fa287d4
jamesacox53 <jamesacox53@gmail.com>
2025-05-31 08:42:58 +0100

    Completed Login Routes

---
+++ 51-YelpCampAddingInAuthentication/05-LoginRoutes/routes/users.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const passport = require('passport');
+
+const router = express.Router();
+
+const catchAsync = require('../utils/catchAsync');
+const User = require('../models/user');
+
+router.get('/register', (req, res) => {
+    res.render('users/register');
+});
+
+router.post('/register', catchAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+
+        const regUser = await User.register(user, password);
+
+        req.flash('success', 'Welcome to YelpCamp!');
+        res.redirect('/campgrounds');
+
+    } catch (err) {
+        req.flash('error', err.message);
+        res.redirect('register');
+    }
+}));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome Back!');
+    res.redirect('/campgrounds');
+});
+
+module.exports = router;
