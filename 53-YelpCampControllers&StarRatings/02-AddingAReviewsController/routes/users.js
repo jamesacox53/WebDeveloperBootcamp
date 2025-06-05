ab5c5c5bd68e8c5aeaacb07193719b63b7677220
jamesacox53 <jamesacox53@gmail.com>
2025-06-05 16:08:34 +0100

    Completed Adding a Reviews Controller

---
+++ 53-YelpCampControllers&StarRatings/02-AddingAReviewsController/routes/users.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const passport = require('passport');
+
+const router = express.Router();
+
+const catchAsync = require('../utils/catchAsync');
+const { storeReturnTo } = require('../middleware');
+
+const users = require('../controllers/users');
+
+router.get('/register', users.renderRegister);
+router.post('/register', catchAsync(users.register));
+router.get('/login', users.renderLogin);
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.get('/logout', users.logout);
+
+module.exports = router;
