f6af124fb3f400050f0fe9235ab5a1fffc31ce0d
jamesacox53 <jamesacox53@gmail.com>
2025-06-19 17:42:15 +0100

    Completed Styling Components

---
+++ 61-JSXInDetail/07-StylingComponents/src/DiceRoll.js
@@ -0,0 +1,13 @@
+import "./DieRoll.css";
+import Die from "./Die";
+
+export default function DiceRoll() {
+  return (
+    <div className="DieRoll">
+      <h1>Dice Roll</h1>
+      <Die />
+      <Die />
+      <Die />
+    </div>
+  );
+}
---
+++ 61-JSXInDetail/07-StylingComponents/src/LoginForm.js
@@ -0,0 +1,9 @@
+export default function LoginForm() {
+  return (
+    <form>
+      <input type="text" />
+      <input type="password" />
+      <button>Login</button>
+    </form>
+  );
+}
