c273873f495cc7719ba788e46faedadb98e1dc5d
jamesacox53 <jamesacox53@gmail.com>
2025-06-19 17:26:51 +0100

    Completed Component Decomposition

---
+++ 61-JSXInDetail/06-ComponentDecomposition/src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+import Greeter from "./Greeter";
+import Dog from "./Dog";
+import LoginForm from "./LoginForm";
+import Die from "./Die";
+import DiceRoll from "./DiceRoll";
+
+export default function App() {
+  return (
+    <div className="App">
+      <DiceRoll />
+      <DiceRoll />
+    </div>
+  );
+}
---
+++ 61-JSXInDetail/06-ComponentDecomposition/src/DiceRoll.js
@@ -0,0 +1,12 @@
+import Die from "./Die";
+
+export default function DiceRoll() {
+  return (
+    <div>
+      <h1>Dice Roll</h1>
+      <Die />
+      <Die />
+      <Die />
+    </div>
+  );
+}
