040f053a44265465e49678841b51a790d3f21321
jamesacox53 <jamesacox53@gmail.com>
2025-06-26 17:25:11 +0100

    Completed Intro to React Events

---
+++ 65-ReactEvents/01-IntroToReactEvents/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Clicker from "./Clicker";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Clicker />
+      <Clicker />
+      <Clicker />
+    </div>
+  );
+}
