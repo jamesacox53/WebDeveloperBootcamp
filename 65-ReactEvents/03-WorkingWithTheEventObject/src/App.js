8d63b0a8d5a41e3e01eb70f732d44f2d495268eb
jamesacox53 <jamesacox53@gmail.com>
2025-06-27 16:54:22 +0100

    Completed Working With The Event Object

---
+++ 65-ReactEvents/03-WorkingWithTheEventObject/src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import Clicker from "./Clicker";
+import Form from "./Form";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Form />
+      <Clicker />
+      <Clicker />
+      <Clicker />
+    </div>
+  );
+}
