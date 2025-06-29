52b961edb11ddd9c2d799af124d79ba28f1a535e
jamesacox53 <jamesacox53@gmail.com>
2025-06-29 19:33:08 +0100

    Completed State Initializer Functions

---
+++ 67-IntermediateStateConcepts/02-StateInitializerFunctions/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Dumbo from "./Dumbo";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>State demo!</h1>
+      <Dumbo />
+    </div>
+  );
+}
