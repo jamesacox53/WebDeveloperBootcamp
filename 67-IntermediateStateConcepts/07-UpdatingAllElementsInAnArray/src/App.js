9d0785783ddbab61e546b7ef983754fbe9b8f0f7
jamesacox53 <jamesacox53@gmail.com>
2025-06-30 17:58:06 +0100

    Completed Updating All Elements In An Array

---
+++ 67-IntermediateStateConcepts/07-UpdatingAllElementsInAnArray/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import EmojiClicker from "./EmojiClicker";
+
+export default function App() {
+  return (
+    <div className="App">
+      <EmojiClicker />
+    </div>
+  );
+}
