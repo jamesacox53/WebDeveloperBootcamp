3b6162130a3b1ae3ec7a720760b448dcbd28127c
jamesacox53 <jamesacox53@gmail.com>
2025-06-30 12:28:59 +0100

    Completed Working With Objects In State

---
+++ 67-IntermediateStateConcepts/03-WorkingWithObjectsInState/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import ScoreKeeper from "./ScoreKeeper";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ScoreKeeper />
+    </div>
+  );
+}
