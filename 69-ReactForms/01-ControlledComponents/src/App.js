1f1cb9f9b7145b4b4356bffc6921719d73202401
jamesacox53 <jamesacox53@gmail.com>
2025-07-01 19:47:27 +0100

    Completed Controlled Components

---
+++ 69-ReactForms/01-ControlledComponents/src/App.js
@@ -0,0 +1,10 @@
+import './styles.css';
+import UsernameForm from './UsernameForm';
+
+export default function App() {
+    return (
+        <div className="App">
+        <UsernameForm />
+        </div>
+    );
+}
