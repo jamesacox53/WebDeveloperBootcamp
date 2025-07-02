834f228a4767dd40832145accedc363ba6212a64
jamesacox53 <jamesacox53@gmail.com>
2025-07-02 07:54:03 +0100

    Completed Working With Multiple Inputs

---
+++ 69-ReactForms/03-WorkingWithMultipleInputs/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import UsernameForm from "./UsernameForm";
+import SignupForm from "./SignupForm";
+
+export default function App() {
+  return (
+    <div className="App">
+      <UsernameForm />
+      <SignupForm />
+    </div>
+  );
+}
