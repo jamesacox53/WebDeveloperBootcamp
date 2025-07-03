1895f6bc4304a65096007c97df811af70a358e1d
jamesacox53 <jamesacox53@gmail.com>
2025-07-03 18:12:31 +0100

    Completed The Rating Component

---
+++ 71-MaterialUI/02-TheRatingComponent/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import ButtonDemo from "./ButtonDemo";
+import RatingDemo from "./RatingDemo";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ButtonDemo />
+      <RatingDemo />
+    </div>
+  );
+}
