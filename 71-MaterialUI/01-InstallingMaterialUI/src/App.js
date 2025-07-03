d3d8aac7180240a6ae93e422a1eab7750001d010
jamesacox53 <jamesacox53@gmail.com>
2025-07-03 17:51:21 +0100

    Completed Installing Material UI

---
+++ 71-MaterialUI/01-InstallingMaterialUI/src/App.js
@@ -0,0 +1,44 @@
+import "./styles.css";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import AlarmIcon from "@mui/icons-material/Alarm";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Button variant="text">Text</Button>
+      <Button variant="contained" onClick={() => alert("Hi")}>
+        Contained
+      </Button>
+      <Button
+        variant="contained"
+        color="success"
+        size="small"
+        onClick={() => alert("Hi")}
+      >
+        Contained
+      </Button>
+      <Button
+        variant="contained"
+        color="error"
+        size="medium"
+        onClick={() => alert("Hi")}
+      >
+        Contained
+      </Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        size="large"
+        onClick={() => alert("Hi")}
+      >
+        Contained
+      </Button>
+      <Button variant="outlined">Outlined</Button>
+
+      <IconButton color="secondary" aria-label="add an alarm">
+        <AlarmIcon />
+      </IconButton>
+    </div>
+  );
+}
