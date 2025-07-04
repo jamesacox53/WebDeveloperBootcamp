25e120c8047063c24b01f7639bda3f0fd8eaaa8c
jamesacox53 <jamesacox53@gmail.com>
2025-07-04 12:22:24 +0100

    Completed Creating The Application

---
+++ 72-BuildingATodoListWithMaterialUI&LocalStorage/01-CreatingTheApplication/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import CssBaseline from "@mui/material/CssBaseline";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+      <h1>Todos</h1>
+    </div>
+  );
+}
